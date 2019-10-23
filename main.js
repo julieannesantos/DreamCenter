3bfa7815993986b636a0e1a2951aa07b3febc253
julie anne santos <julie.santos@students.jmcss.org>
2019-10-23 08:33:56 -0500

    :)

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $(".dropdown-toggle").dropdown();
+});
+$('#carouselExampleIndicators').carousel({
+    interval: 3000,
+    pause: null
+  })
+  $(".left").click(function(){
+    $("#carouselExampleIndicators").carousel("prev");
+  });
+  $(".right").click(function(){
+    $("#carouselExampleIndicators").carousel("next");
+  });
+});
