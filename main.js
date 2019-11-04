24723504e9e8c6eabbd8b7e0ec2f1639e5b84cf7
julie anne santos <julie.santos@students.jmcss.org>
2019-11-04 08:46:07 -0600

    update

--- main.js
+++ main.js
@@ -11,3 +11,22 @@ $('#carouselExampleIndicators').carousel({
   $(".right").click(function(){
     $("#carouselExampleIndicators").carousel("next");
   });
+
+
+  //mobile navbar button animation
+
+  $(document).ready(function () {
+
+    $('.first-button').on('click', function () {
+  
+      $('.animated-icon1').toggleClass('open');
+    });
+    $('.second-button').on('click', function () {
+  
+      $('.animated-icon2').toggleClass('open');
+    });
+    $('.third-button').on('click', function () {
+  
+      $('.animated-icon3').toggleClass('open');
+    });
+  });
