4ee31694d5e4b088397f342ab5c8dbfa5865d4a0
julie anne santos <julie.santos@students.jmcss.org>
2019-11-21 08:44:54 -0600

    5

--- main.js
+++ main.js
@@ -1,58 +1,58 @@
 $(document).ready(function() {
-    $(".dropdown-toggle").dropdown();
+  $(".dropdown-toggle").dropdown();
 });
 $('#carouselExampleIndicators').carousel({
-    interval: 3000,
-    pause: null
-  })
-  $(".left").click(function(){
-    $("#carouselExampleIndicators").carousel("prev");
+  interval: 3000,
+  pause: null
+})
+$(".left").click(function() {
+  $("#carouselExampleIndicators").carousel("prev");
+});
+$(".right").click(function() {
+  $("#carouselExampleIndicators").carousel("next");
+});
+
+
+//mobile navbar button animation
+
+$(document).ready(function() {
+
+  $('.first-button').on('click', function() {
+
+    $('.animated-icon1').toggleClass('open');
   });
-  $(".right").click(function(){
-    $("#carouselExampleIndicators").carousel("next");
+  $('.second-button').on('click', function() {
+
+    $('.animated-icon2').toggleClass('open');
   });
+  $('.third-button').on('click', function() {
 
-
-  //mobile navbar button animation
-
-  $(document).ready(function () {
-
-    $('.first-button').on('click', function () {
-  
-      $('.animated-icon1').toggleClass('open');
-    });
-    $('.second-button').on('click', function () {
-  
-      $('.animated-icon2').toggleClass('open');
-    });
-    $('.third-button').on('click', function () {
-  
-      $('.animated-icon3').toggleClass('open');
-    });
+    $('.animated-icon3').toggleClass('open');
   });
-  // contact
-  $('.contact-form').find('.form-control').each(function() {
-    var targetItem = $(this).parent();
-    if ($(this).val()) {
-      $(targetItem).find('label').css({
-        'top': '10px',
-        'fontSize': '14px'
-      });
-    }
-  })
-  $('.contact-form').find('.form-control').focus(function() {
-    $(this).parent('.input-block').addClass('focus');
-    $(this).parent().find('label').animate({
+});
+// contact
+$('.contact-form').find('.form-control').each(function() {
+  var targetItem = $(this).parent();
+  if ($(this).val()) {
+    $(targetItem).find('label').css({
       'top': '10px',
       'fontSize': '14px'
+    });
+  }
+})
+$('.contact-form').find('.form-control').focus(function() {
+  $(this).parent('.input-block').addClass('focus');
+  $(this).parent().find('label').animate({
+    'top': '10px',
+    'fontSize': '14px'
+  }, 300);
+})
+$('.contact-form').find('.form-control').blur(function() {
+  if ($(this).val().length == 0) {
+    $(this).parent('.input-block').removeClass('focus');
+    $(this).parent().find('label').animate({
+      'top': '25px',
+      'fontSize': '18px'
     }, 300);
-  })
-  $('.contact-form').find('.form-control').blur(function() {
-    if ($(this).val().length == 0) {
-      $(this).parent('.input-block').removeClass('focus');
-      $(this).parent().find('label').animate({
-        'top': '25px',
-        'fontSize': '18px'
-      }, 300);
-    }
-  })
+  }
+})
